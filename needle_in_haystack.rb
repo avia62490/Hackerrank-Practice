4844fd5d57721cade3f5945a1acc1500cc7faac8
Alex Via <alexandervia@gmail.com>
2023-06-15 11:07:35 -0500

    completes exercise

---
+++ needle_in_haystack.rb
@@ -0,0 +1,8 @@
+# Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+# @param {String} haystack
+# @param {String} needle
+# @return {Integer}
+def str_str(haystack, needle)
+  return haystack.include?(needle) ? haystack.index(needle) : -1
+end
