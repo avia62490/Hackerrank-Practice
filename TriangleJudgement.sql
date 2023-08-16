f3356c499722160f501208dfc0b9b68a4291a3a3
Alex Via <alexandervia@gmail.com>
2023-08-16 11:37:22 -0500

    completes exercise

---
+++ TriangleJudgement.sql
@@ -0,0 +1,34 @@
+-- Report for every three line segments whether they can form a triangle.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+-- Example 1:
+
+-- Input: 
+-- Triangle table:
+-- +----+----+----+
+-- | x  | y  | z  |
+-- +----+----+----+
+-- | 13 | 15 | 30 |
+-- | 10 | 20 | 15 |
+-- +----+----+----+
+-- Output: 
+-- +----+----+----+----------+
+-- | x  | y  | z  | triangle |
+-- +----+----+----+----------+
+-- | 13 | 15 | 30 | No       |
+-- | 10 | 20 | 15 | Yes      |
+-- +----+----+----+----------+
+
+-- # Write your MySQL query statement below
+SELECT
+  x,
+  y,
+  z,
+  CASE
+    WHEN x + y <= z OR x + z <= y OR y + z <= x THEN 'No'
+    ELSE 'Yes'
+  END AS triangle
+FROM triangle
